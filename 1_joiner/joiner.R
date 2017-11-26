e0601e50854094d3745b78baad79b6fb3c3abedf
Roberto Salas <wala.rsalas@gmail.com>
2017-11-25 21:01:04 -0300

    More work on joiner

--- 1_joiner/joiner.R
+++ 1_joiner/joiner.R
@@ -5,6 +5,42 @@ barrios_p <- readRDS("Data/barrios_pil.RDS")
 
 manzbarr <- readRDS("Data/Manzanasbarriospiloto.RDS")
 
+
+barrios <- read_delim("Id,BARRIO,CIUDAD,COMUNA
+                      0,Barrio Amanecer,Temuco - Padre Las Casas,Temuco
+                      0,Barrio Estación,Temuco - Padre Las Casas,Temuco
+                      0,Camilo Henriquez,Gran Concepción,Concepción
+                      0,Fundo El Carmen,Temuco - Padre Las Casas,Temuco
+                      0,Lomas de San Sebastian,Gran Concepción,Concepción
+                      0,Villa Los Fundadores,Valdivia,Valdivia
+                      0,Parque Krahmer,Valdivia,Valdivia
+                      0,Sector Regional,Valdivia,Valdivia
+                      0,Barrio Brasil,Gran Santiago,Santiago
+                      0,Villa Codelco,Copiapó,Copiapó
+                      0,Tierra Viva Oriente,Copiapó,Copiapó
+                      0,El Llano,Gran Coquimbo,Coquimbo
+                      0,Villa Vista Hermosa,Gran Coquimbo,La Serena
+                      0,Barrio San Miguel,Gran Santiago,San Miguel
+                      0,Barrio Las Lilas,Gran Santiago,Providencia
+                      0,Barrio Jardin del Este,Gran Santiago,Vitacura
+                      0,Barrio Plaza de Maipú,Gran Santiago,Maipú
+                      0,Barrios Bajos,Valdivia,Valdivia
+                      0,Guayacán,Gran Coquimbo,Coquimbo
+                      0,Villa Esperanza,Copiapó,Copiapó
+                      0,El Faro Parte Alta,Gran Coquimbo,Coquimbo
+                      0,Juan XXIII,Gran Coquimbo,La Serena
+                      0,Chorrillos,Gran Santiago,Independencia
+                      0,Brasilia,Gran Santiago,San Miguel
+                      0,El Mariscal,Gran Santiago,Puente Alto
+                      0,U.V. 35 José María Caro,Gran Santiago,Lo Espejo
+                      0,Pucará de Lasana,Gran Santiago,Quilicura
+                      0,Juan González Huerta,Gran Concepción,Talcahuano
+                      0,Cerro Verde Alto,Gran Concepción,Penco
+                      0,Leonera 2,Gran Concepción,Chiguayante
+                      0,Padre Hurtado (CCSS),Temuco - Padre Las Casas,Temuco
+                      0,Las Quilas,Temuco - Padre Las Casas,Temuco", delim = ",")
+
+
 vars <- data.frame(
   bigname = c(
     "% Empleados en la misma comuna",
@@ -61,6 +97,7 @@ dataFunc <- function(a, b, c) {
     t <- readRDS(as.character(a))[,c("ID_W","value")]
     t <- filter(t, t$ID_W %in% as.character(barrios_m@data$MANZENT))
     t$d <- barrios_m@data[match(t$ID_W,as.character(barrios_m@data$MANZENT)),]$BARRIO
+    t <- left_join(t,barrios,by=c("d" = "BARRIO"))
     t$codename <- rep(as.character(b), nrow(t))
     t$bigname <- rep(as.character(c), nrow(t))
     return(t)
@@ -68,3 +105,4 @@ dataFunc <- function(a, b, c) {
 }
 
 data <- apply(vars, 1, function(x) dataFunc(x['path'],x['codename'],x['bigname']))
+data <- bind_rows(data)
